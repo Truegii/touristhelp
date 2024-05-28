10f514dc38910200bfe5e98d97d45bb6ab1b7bfc
Truegii <72995319+Truegii@users.noreply.github.com>
2024-05-28 13:29:08 -0500

    Add files via upload

---
+++ BDTouristHelp/buscaitem.php
@@ -0,0 +1,26 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+		require_once("db.php");
+
+		$result = array();
+        $query = "SELECT * FROM zonas";
+        $responce = $mysql->query($query);
+
+        while($row = mysqli_fetch_array($responce))
+        {
+            $index['id']= $row['0'];
+            $index['nombre']= $row['1'];
+            $index['desc']= $row['2'];
+            $index['imgurl']= $row['3'];
+            $index['depa']= $row['4'];
+            $index['califica']= $row['5'];
+            $index['direc']= $row['6'];
+
+            array_push($result, $index);
+
+        }
+
+        echo json_encode($result);
+
+	}
+?>
